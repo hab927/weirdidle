9ceb3e5426096fc886a251fab5c8819d7f0bc1e0
hab927 <warmairballoon@gmail.com>
2024-05-29 22:49:11 -0700

    added some "things"

--- upgrade.js
+++ upgrade.js
@@ -1,7 +1,27 @@
-var counter = 0;
-var mainNumHTML = document.getElementById("counter").innerHTML;
+var mainNumHTML = document.getElementById("counter");
+var upgradeButton = document.getElementById("increase");
+
+// game variables
+var count = 0;
+var countPerSecond = 1;
+var tickspeed = 1000;
+var tick;
+
+var $tspeed = document.querySelector('#tspeed');
 
 function increase() {
-    counter = counter + 1;
-    mainNumHTML.innerHTML = counter;
+    count = count + 1;
+    mainNumHTML.innerHTML = count;
+}
+
+function decreaseInterval() {
+    clearInterval(tick);
+    tickspeed -= 1;
+    $tspeed.innerHTML -= 1;
+    tick = setInterval(automatic, tickspeed);
+}
+
+function automatic() {
+    count++;
+    mainNumHTML.innerHTML = count;
 }
